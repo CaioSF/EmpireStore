bbdc1f51331138819c1e9f5f2524ebbfda73fe7e
Pedro Sudre <pedrosudre04@gmail.com>
2022-09-26 15:37:06 -0300

    Mudança no titulo

--- aplic/models.py
+++ aplic/models.py
@@ -1,12 +1,11 @@
 from django.db import models
-from django import forms
+
 
 
 class Produto(models.Model):
     cod_produto = models.CharField('Codigo', max_length=100)
     descricao = models.TextField('Descrição', max_length=500)
     lote = models.CharField('Lote', max_length=30)
-
 
 
     class Meta:
