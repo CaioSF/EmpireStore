a1fa914ae3c115a8efa6ab8a0b6b3a5ea914b6c6
Pedro Sudre <pedrosudre04@gmail.com>
2022-09-28 01:03:32 -0300

    Atualização nas classes

--- aplic/admin.py
+++ aplic/admin.py
@@ -1,23 +1,25 @@
 from django.contrib import admin
 
-from .models import Produto, Compra, Cliente, Funcionario
-
-@admin.register(Produto)
-class ProdutoAdmin(admin.ModelAdmin):
-    list_display = ('cod_produto', 'descricao', 'lote')
-
+from .models import Produto, Compra, Cliente, Funcionario, Endereco
 
 @admin.register(Compra)
 class CompraAdmin(admin.ModelAdmin):
-    list_display = ('valor', 'quantidade', 'cod_produto')
+    list_display = ('valor', 'quantidade')
 
 @admin.register(Cliente)
 class ClienteAdmin(admin.ModelAdmin):
-    list_display = ('nome', 'cpf', 'data_nascimento', 'id_cliente', 'email', 'end_logradouro', 'end_numero', 'end_cep',
-                    'end_bairro', 'end_uf', 'end_cidade', 'end_complemento')
+    list_display = ('nome', 'cpf', 'data_nascimento')
 
 @admin.register(Funcionario)
 class FuncionarioAdmin(admin.ModelAdmin):
-    list_display = ('nome', 'cpf', 'data_nascimento', 'id_funcionario', 'cargo', 'end_logradouro', 'end_numero', 'end_cep',
-                    'end_bairro', 'end_uf', 'end_cidade', 'end_complemento')
+    list_display = ('nome', 'cpf', 'data_nascimento', 'cargo')
 
+
+@admin.register(Produto)
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ('tipo', 'marca', 'modelo', 'descricao', 'lote')
+
+
+@admin.register(Endereco)
+class EnderecoAdmin(admin.ModelAdmin):
+    list_display = ('end_logradouro', 'end_numero', 'end_cep', 'end_bairro', 'end_uf', 'end_cidade', 'end_complemento')
--- aplic/models.py
+++ aplic/models.py
@@ -1,9 +1,8 @@
 from django.db import models
-from django.db.models import Model
 
 class Endereco(models.Model):
     end_logradouro = models.CharField('Logradouro', max_length=200)
-    end_numero = models.CharField('Número', blank=True, max_length=10)
+    end_numero = models.CharField('Numero', blank=True, max_length=10)
     end_cep = models.CharField('CEP', max_length=10)
     end_bairro = models.CharField('Bairro', max_length=30)
     end_uf = models.CharField('UF', max_length=2)
@@ -24,7 +23,6 @@ class Usuario(models.Model):
     cpf = models.CharField('CPF', max_length=11)
     data_nascimento = models.DateField('Data de Nascimento', blank=True, null=True, help_text='Formato DD/MM/AAAA')
 
-
     class Meta:
         abstract = True
         verbose_name = 'Usuário'
@@ -33,35 +31,45 @@ class Usuario(models.Model):
     def __str__(self):
         return self.nome
 
-class Cliente(Usuario, Endereco):
-    id_cliente = models.CharField('ID', max_length=5)
+class Cliente(Usuario):
     email = models.EmailField('email', max_length=200)
-    endereco = models.ForeignKey(Endereco, on_delete=models.PROTECT, related_name="clientes")
+    endereco = models.ForeignKey(Endereco, null=True, on_delete=models.DO_NOTHING)
 
     class Meta:
         verbose_name = 'Cliente'
         verbose_name_plural = 'Clientes'
 
-class Funcionario(Usuario, Endereco):
-    id_funcionario = models.CharField('ID', max_length=5)
+    def __str__(self):
+        return self.nome
+
+class Funcionario(Usuario):
     OPCOES = (
         ('Gerente', 'Gerente'),
         ('Atendente', 'Atendente'),
     )
     cargo = models.CharField('Cargo', blank=True, max_length=100, choices=OPCOES)
-    endereco = models.ForeignKey(Endereco, on_delete=models.PROTECT, related_name="funcionarios")
+    endereco = models.ForeignKey(Endereco, null=True, on_delete=models.DO_NOTHING)
 
     class Meta:
-        verbose_name = 'Funcionário'
-        verbose_name_plural = 'Funcionários'
+        verbose_name = 'Funcionario'
+        verbose_name_plural = 'Funcionarios'
 
     def __str__(self):
         return self.nome
 
-
 class Produto(models.Model):
-    cod_produto = models.CharField('Código', max_length=100)
-    descricao = models.TextField('Descrição', max_length=500)
+    OPCOES = (
+        ('Mouse', 'Mouse'),
+        ('Teclado', 'Teclado'),
+        ('Headset', 'Headset'),
+        ('Controle', 'Controle'),
+        ('Impressora', 'Impressora'),
+        ('Caixa de Som', 'Caixa de Som'),
+    )
+    tipo = models.CharField('Tipo', blank=True, max_length=20, choices=OPCOES)
+    marca = models.CharField('Marca', max_length=50)
+    modelo = models.CharField('Modelo', max_length=30)
+    descricao = models.TextField('Descricao', max_length=500)
     lote = models.CharField('Lote', max_length=30)
 
 
@@ -69,17 +77,30 @@ class Produto(models.Model):
         verbose_name = 'Produto'
         verbose_name_plural = 'Produtos'
 
+
     def __str__(self):
-        return self.cod_produto
+        return f"{self.tipo} {self.marca} {self.modelo}"
 
+class Fornecedor(models.Model):
+    cnpj = models.CharField('CNPJ', max_length=14)
+    inscricao_estadual = models.CharField('Inscrição Estadual', max_length=9)
+    razao_social = models.CharField('Razão Social', max_length=50)
+    contato = models.EmailField('Contato', max_length=100, help_text='contato@exemplo.com')
 
-class Compra(Model):
-    valor = models.DecimalField('Preço', max_digits=5, decimal_places=2)
+    class Meta:
+        verbose_name = 'Fornecedor'
+        verbose_name_plural = 'Fornecedores'
+
+    def __str__(self):
+        return self.razao_social
+
+class Compra(models.Model):
+    valor = models.DecimalField('Preco', max_digits=5, decimal_places=2)
     quantidade = models.IntegerField('Quantidade')
-    cod_produto = models.CharField('Código', max_length=20)
 
     class Meta:
         verbose_name = "Compra"
         verbose_name_plural = "Compras"
+
 
 
