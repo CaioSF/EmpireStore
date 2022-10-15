d9105f9eb57a7fb33c6943304c61ab863dd3e15f
Pedro Sudre <pedrosudre04@gmail.com>
2022-10-15 14:48:05 -0300

    Atualizações diversas

--- emp/settings.py
+++ emp/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '+_+ug11y*_0aku6oapl)o++ux9cf-5rz&7%kvultl_$=#o_%)g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
