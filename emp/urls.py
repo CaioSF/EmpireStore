373adea7960a38fa73610ca87d0e70d5ca1f8a4f
Pedro Sudré <pedrosudre04@gmail.com>
2022-11-10 09:12:25 -0300

    search updates

--- emp/urls.py
+++ emp/urls.py
@@ -7,4 +7,5 @@ from django.conf import settings
 urlpatterns = [
     path('restrito/', admin.site.urls),
     path('', include('aplic.urls')),
+    path('search', include("search.urls", namespace="search"))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
