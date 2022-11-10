8c5f74a99776b7393b36ee32fe9302174d4f28a7
Pedro Sudré <pedrosudre04@gmail.com>
2022-11-10 09:45:56 -0300

    search/views.py update

--- search/views.py
+++ search/views.py
@@ -6,10 +6,9 @@ class SearchProductView(ListView):
     template_name = "search/view.html"
 
     def get_context_data(self, *args, **kwargs):
-        context = super(SearchProductView, self).get_context_data(*args, **kwargs)
+        context = super().get_context_data(*args, **kwargs)
         query = self.request.GET.get('q')
         context['query'] = query
-        #SearchQuery.objects.create(query=query)
         return context
 
     def get_queryset(self, *args, **kargs):
@@ -17,7 +16,7 @@ class SearchProductView(ListView):
         print('Solicitação', request)
         result = request.GET
         print('Resultado: ', result)
-        query = result.get('q',  None) # method['q']
+        query = result.get('q',  None) 
         print('Consulta', query)
         if query is not None:
             return Product.objects.filter(title__contains = query)
