f18ed2ef4cb0a08fb4600cf18c8e5573d14b233e
Pedro Sudré <pedrosudre04@gmail.com>
2022-11-10 22:13:41 -0300

    slug correction

--- aplic/utils.py
+++ aplic/utils.py
@@ -16,7 +16,7 @@ def unique_slug_generator(instance, new_slug=None):
     if new_slug is not None:
         slug = new_slug
     else:
-        slug = slugify(instance.modelo)
+        slug = slugify(instance.title)
 
     Klass = instance.__class__
     qs_exists = Klass.objects.filter(slug = slug).exists()
