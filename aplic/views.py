6291a4aa1cf22e8ce3df45e4cc0087ad2e744c28
Pedro Sudré <pedrosudre04@gmail.com>
2022-11-12 14:37:07 -0300

    ok

--- aplic/views.py
+++ aplic/views.py
@@ -145,5 +145,6 @@ def cadastro(request):
         new_user = User.objects.create_user(username, email, password)
         print(new_user)
     return render(request, "auth/entrar.html", context)
+
     
     
