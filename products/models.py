15e694bd49fc7a6ef73159f8e28664ae67f0366d
Pedro Sudré <pedrosudre04@gmail.com>
2022-11-15 08:02:38 -0300

    updates

---
+++ products/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class Product(models.Model):
+    title = models.CharField('Títutlo', max_length=120)
+    description = models.TextField('Description', max_length=None, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
