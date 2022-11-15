8d1e4f9237ea31b7931b305693570207d5984d5b
Pedro Sudré <pedrosudre04@gmail.com>
2022-11-15 11:59:22 -0300

    create search app

--- empirestore/settings.py
+++ empirestore/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     # our apps
     'aplic',
     'products',
+    'search',
 ]
 
 
--- empirestore/urls.py
+++ empirestore/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('aplic.urls')),
     path('products/', include("products.urls", namespace="products")),
+    path('search/', include("search.urls", namespace="search")),
 ]
