ace5f9bf2956c6d25094e1b9fcebb547dc5b9d45
Pedro Sudré <pedrosudre04@gmail.com>
2022-11-15 17:14:55 -0300

    update orders

--- orders/models.py
+++ orders/models.py
@@ -12,6 +12,22 @@ ORDER_STATUS_CHOICES = (
     ('refunded', 'Devolvido'),
 )
 
+class OrderManager(models.Manager):
+    def new_or_get(self, billing_profile, cart_obj):
+        created = False
+        qs = self.get_queryset().filter(
+                billing_profile = billing_profile, 
+                cart = cart_obj, 
+                active = True)
+        if qs.count() == 1:
+            obj = qs.first()
+        else:
+            obj = self.model.objects.create(
+                    billing_profile = billing_profile, 
+                    cart=cart_obj)
+            created = True
+        return obj, created
+
 class Order(models.Model):
     billing_profile = models.ForeignKey(BillingProfile, on_delete=models.CASCADE, null = True, blank = True)
     order_id = models.CharField(max_length = 120, blank = True)
@@ -27,6 +43,8 @@ class Order(models.Model):
     def __str__(self):
         return self.order_id
 
+    objects = OrderManager()
+
     def update_total(self):
         cart_total = self.cart.total
         shipping_total = self.shipping_total
@@ -38,6 +56,14 @@ class Order(models.Model):
 def pre_save_create_order_id(sender, instance, *args, **kwargs):
     if not instance.order_id:
         instance.order_id = unique_order_id_generator(instance)
+
+    # retorna todos os orders com esta instância de cart, 
+    # excluindo aqueles que têm a mesma instância de billing_profile
+    qs = Order.objects.filter(cart = instance.cart).exclude(billing_profile = instance.billing_profile)
+    if qs.exists():
+        qs.update = False
+
+
 
 pre_save.connect(pre_save_create_order_id, sender = Order)
 
