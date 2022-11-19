451dd8d5ba739e2474327cdc169f2c2537dd35a9
Pedro Sudré <pedrosudre04@gmail.com>
2022-11-19 17:41:08 -0300

    fix

--- empirestore/settings.py
+++ empirestore/settings.py
@@ -133,6 +133,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    'static/',
+]
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
