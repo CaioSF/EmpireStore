f280d1ad87c371a50fd3830cf4cd77839f8dbb67
Pedro Sudré <pedrosudre04@gmail.com>
2022-11-20 08:22:02 -0300

    fix in search and pages

--- orders/models.py
+++ orders/models.py
@@ -38,7 +38,7 @@ class Order(models.Model):
     billing_address = models.ForeignKey(Address, related_name="billing_address", on_delete=models.CASCADE, null=True, blank=True)
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE, null = True)
     status = models.CharField(max_length = 120, default = 'created', choices = ORDER_STATUS_CHOICES )
-    shipping_total = models.DecimalField(default = 5.99, max_digits = 100, decimal_places = 2)
+    shipping_total = models.DecimalField(default = 20.00, max_digits = 100, decimal_places = 2)
     total = models.DecimalField(default = 0.00, max_digits = 100, decimal_places = 2)
     active = models.BooleanField(default=True)
 
