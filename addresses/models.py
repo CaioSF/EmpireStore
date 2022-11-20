70afb4f279f6b6cf870b9e7c4fc42a491813ed47
Pedro Sudré <pedrosudre04@gmail.com>
2022-11-20 08:04:06 -0300

    update addresses

--- addresses/models.py
+++ addresses/models.py
@@ -2,19 +2,19 @@ from django.db import models
 
 from billing.models import BillingProfile
 
-ADDRESS_TYPES = (
+OPCOES = (
     ('billing', 'Billing'),
     ('shipping', 'Shipping'),
 )
 class Address(models.Model):
     billing_profile = models.ForeignKey(BillingProfile, on_delete=models.CASCADE, null = True, blank = True)
-    address_type    = models.CharField(max_length = 120, choices = ADDRESS_TYPES)
-    address_line_1  = models.CharField(max_length = 120)
-    address_line_2  = models.CharField(max_length = 120, null = True, blank = True)
-    city            = models.CharField(max_length = 120)
-    country         = models.CharField(max_length = 120, default = 'Brazil')
-    state           = models.CharField(max_length = 120)
-    postal_code     = models.CharField(max_length = 120)
+    address_type    = models.CharField(max_length = 120, choices = OPCOES)
+    address_line_1  = models.CharField("Endereço 1", max_length = 120)
+    address_line_2  = models.CharField("Endereço 2", max_length = 120, null = True, blank = True)
+    city            = models.CharField("Cidade", max_length = 120)
+    country         = models.CharField("País", max_length = 120, default = 'Brasil')
+    state           = models.CharField("Estado", max_length = 120)
+    postal_code     = models.CharField("CEP", max_length = 120)
 
     def __str__(self):
         return str(self.billing_profile)
