5c819f91cdbe0639ac54df462357c18fc1c93179
caioSF <caios8837@gmail.com>
2022-11-24 15:03:02 -0300

    paginação

--- products/urls.py
+++ products/urls.py
@@ -8,6 +8,7 @@ from .views import (
                     )
 
 urlpatterns = [
+    
     path('', ProductListView.as_view(), name='list'),
     path('<slug:slug>/', ProductDetailSlugView.as_view(), name='detail')
 ]
--- products/views.py
+++ products/views.py
@@ -2,12 +2,13 @@ from django.http import Http404
 
 from django.views.generic import ListView, DetailView
 from django.shortcuts import render, get_object_or_404
-
+from django.core.paginator import Paginator
 from carts.models import Cart
 from .models import Product
 
 class ProductFeaturedListView(ListView):
     template_name = "products/list.html"
+    
     
     def get_queryset(self, *args, **kwargs):
         request = self.request
@@ -22,11 +23,11 @@ class ProductListView(ListView):
     #traz todos os produtos do banco de dados sem filtrar nada 
     queryset = Product.objects.all()
     template_name = "products/list.html"
+    paginate_by = 3
+    
 
-    # def get_context_data(self, *args, **kwargs):
-    #     context = super(ProductListView, self).get_context_data(*args, **kwargs)
-    #     print(context)
-    #     return context
+    
+    
 
     def get_context_data(self, *args, **kwargs):
         context = super(ProductListView,  self).get_context_data(*args, **kwargs)
@@ -37,6 +38,10 @@ class ProductListView(ListView):
 #Function Based View
 def product_list_view(request):
     queryset = Product.objects.all()
+    
+    p = Paginator(Product.objects.all(), 3)
+    page = request.GET.get(page)
+    produto = p.get_page(page)
     context = {
         'object_list': queryset
     }
